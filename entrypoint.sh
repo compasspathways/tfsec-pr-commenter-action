a623e88ae8b4c9c9e642324fc9be8cdd02c3cbd1
Tamás Kádár <ktamas@ktamas.com>
2022-06-30 16:18:45 +0100

    Support arm64

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,30 +1,43 @@
 #!/bin/bash
-
 set -xe
 
-TFSEC_VERSION=""
-if [ "$INPUT_TFSEC_VERSION" != "latest" ]; then
-  TFSEC_VERSION="/tags/${INPUT_TFSEC_VERSION}"
+ARCH=$(uname -m)
+if [ "${ARCH}" == "x86_64" ]; then
+  ARCH="amd64"
 fi
 
-wget -O - -q "$(wget -q https://api.github.com/repos/aquasecurity/tfsec/releases${TFSEC_VERSION} -O - | grep -m 1 -o -E "https://.+?tfsec-linux-amd64" | head -n1)" > tfsec-linux-amd64
-wget -O - -q "$(wget -q https://api.github.com/repos/aquasecurity/tfsec/releases${TFSEC_VERSION} -O - | grep -m 1 -o -E "https://.+?tfsec_checksums.txt" | head -n1)" > tfsec.checksums
+if [ "${ARCH}" == "aarch64" ] || [ "${ARCH}" == "arm64" ]; then
+  ARCH="arm64"
+fi
 
-grep tfsec-linux-amd64 tfsec.checksums > tfsec-linux-amd64.checksum
-sha256sum -c tfsec-linux-amd64.checksum
-install tfsec-linux-amd64 /usr/local/bin/tfsec
+# default to latest
+TFSEC_VERSION="latest"
+
+# if INPUT_TFSEC_VERSION set and not latests
+if [ -n "${INPUT_TFSEC_VERSION}" ] && [ "${INPUT_TFSEC_VERSION}" != "latest" ]; then
+  TFSEC_VERSION="tags/${INPUT_TFSEC_VERSION}"
+fi
+
+# Pull https://api.github.com/repos/aquasecurity/tfsec/releases for the full list of releases. NOTE no trailing slash
+wget -O - -q "$(wget -q https://api.github.com/repos/aquasecurity/tfsec/releases/${TFSEC_VERSION} -O - | grep -m 1 -o -E "https://.+?tfsec-linux-${ARCH}" | head -n1)" > "tfsec-linux-${ARCH}"
+wget -O - -q "$(wget -q https://api.github.com/repos/aquasecurity/tfsec/releases/${TFSEC_VERSION} -O - | grep -m 1 -o -E "https://.+?tfsec_checksums.txt" | head -n1)" > tfsec.checksums
+
+# pipe out the checksum and validate
+grep "tfsec-linux-${ARCH}" tfsec.checksums > "tfsec-linux-${ARCH}.checksum"
+sha256sum -c "tfsec-linux-${ARCH}.checksum"
+install "tfsec-linux-${ARCH}" /usr/local/bin/tfsec
 
 COMMENTER_VERSION="latest"
 if [ "$INPUT_COMMENTER_VERSION" != "latest" ]; then
   COMMENTER_VERSION="tags/${INPUT_COMMENTER_VERSION}"
 fi
 
-wget -O - -q "$(wget -q https://api.github.com/repos/aquasecurity/tfsec-pr-commenter-action/releases/${COMMENTER_VERSION} -O - | grep -o -E "https://.+?commenter-linux-amd64")" > commenter-linux-amd64
-wget -O - -q "$(wget -q https://api.github.com/repos/aquasecurity/tfsec-pr-commenter-action/releases/${COMMENTER_VERSION} -O - | grep -o -E "https://.+?checksums.txt")" > commenter.checksums
+wget -O - -q "$(wget -q https://api.github.com/repos/compasspathways/tfsec-pr-commenter-action/releases/${COMMENTER_VERSION} -O - | grep -o -E "https://.+?commenter-linux-${ARCH}")" > "commenter-linux-${ARCH}"
+wget -O - -q "$(wget -q https://api.github.com/repos/compasspathways/tfsec-pr-commenter-action/releases/${COMMENTER_VERSION} -O - | grep -o -E "https://.+?checksums.txt")" > commenter.checksums
 
-grep commenter-linux-amd64 commenter.checksums > commenter-linux-amd64.checksum
-sha256sum -c commenter-linux-amd64.checksum
-install commenter-linux-amd64 /usr/local/bin/commenter
+grep "commenter-linux-${ARCH}" commenter.checksums > "commenter-linux-${ARCH}.checksum"
+sha256sum -c "commenter-linux-${ARCH}.checksum"
+install "commenter-linux-${ARCH}" /usr/local/bin/commenter
 
 if [ -n "${GITHUB_WORKSPACE}" ]; then
   cd "${GITHUB_WORKSPACE}" || exit
